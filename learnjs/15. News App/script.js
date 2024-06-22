22a746d01f08b26cb60d5058b093be2cfaa901b9
atuldhakhaiya <atuldhakhaiya@gmail.com>
2024-06-22 11:23:42 +0530

    Update script.js

--- learnjs/15. News App/script.js
+++ learnjs/15. News App/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "9545bc4a3fc44731844208a1c3bfef93";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
